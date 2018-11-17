cdbd9c4fa1a5c1750f9646c3fe9514c2293a6fb6
eseuteo <ricardoholthausen@gmail.com>
2018-11-17 18:00:03 +0100

    Exercise 1 first approach
    This would solve the problem but I'm not sure it is the proper way of doing it

--- robotics/practice_4/exercise_1.py
+++ robotics/practice_4/exercise_1.py
@@ -1,6 +1,7 @@
 import auxiliary_functions as af
 import numpy as np
 import matplotlib.pyplot as plt
+from functools import reduce
 
 # Map/landmarks related
 nLandmarks = 7
@@ -21,7 +22,7 @@ xOdom = np.zeros(shape=(3,1))
 fig = plt.figure(1)
 ax = fig.add_subplot(111, aspect='equal')
 plt.grid(axis='both')
-plt.plot(Map[0,:], Map[1,:], 'sm', lw=1, label='LandMarks')
+plt.plot(Map[0,:], Map[1,:], 'sm', lw=1, label='LandMarks', mfc='none', mew=2)
 plt.xlabel('x (m)')
 plt.ylabel('y (m)')
 
@@ -54,8 +55,41 @@ jH = np.zeros(shape=(nLandmarks, 2))
 
 xEst = xOdom
 
-# while np.norm(incr) > tolerance and iteration < nIterations:
-#     plot(xEst[0], xEst[1], '+r', ms=(1 + np.floor((iteration * 15)/nIterations)))
+while np.linalg.norm(incr) > tolerance and iteration < nIterations:
+    plt.plot(xEst[0], xEst[1], '+r', ms=(1 + np.floor((iteration * 15)/nIterations)))
 
+    est_observations = []
+    for kk in range(nLandmarks):
+        # Take an observation to each landmark, i.e.: compute distance to
+        # each one (RANGE sensor affected by gaussian noise)
+        _distance_kk = af.euclid_distance(xEst, af.get_pose(Map[:, kk]))[0]
+        _distance_kk += np.random.randn() * var_d
+        est_observations.append(_distance_kk)
+
+    e = np.array(observations) - np.array(est_observations)
+    residual = np.sqrt(np.matmul(e.T, e))
+
+    # 2) Compute the Jacobians with respect (x, y)
+    # The jH is evaluated at out current guest (xEst) -> z_p
+
+    for kk in range(nLandmarks):
+        jH[kk,0] = -1/(est_observations[kk]) * (Map[0, kk] - xEst[0])
+        jH[kk,1] = -1/(est_observations[kk]) * (Map[1, kk] - xEst[1])
+
+    _d1 = np.linalg.inv(np.matmul(jH.T, jH))
+    delta = reduce(np.matmul, [_d1, jH.T, e])
+
+    R = np.diag(var_d * np.sqrt(z))
+
+    incr = delta
+
+    plt.plot([xEst[0], xEst[0] + incr[0]], [xEst[1], xEst[1] + incr[1]], 'r')
+    incr.shape = (2,1)
+    xEst[0:2] = xEst[0:2] + incr
+    print('Iteration number ' + str(iteration+1) + ' residual: ' + str(residual) + ' [m] increment: ' + str(incr) + ' [m]')
+    iteration += 1
+    input('Press Enter to continue...')
+
+plt.plot(xEst[0], xEst[1], 'g*')
 plt.legend()
 plt.show()
